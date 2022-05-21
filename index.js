16f1b0ed651f603d99f1639f4285353b98bb2c02
Istiak <istieak.hasan1996@gmail.com>
2022-05-21 17:01:36 +0600

    all done

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express=require('express')
 const app=express()
 const cors=require('cors');
 const jwt=require('jsonwebtoken')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 var nodemailer = require('nodemailer');
 var sendinBlue = require('nodemailer-sendinblue-transport');
+
 app.use(cors())
 require('dotenv').config()
+const stripe=require('stripe')(process.env.STRIPE_SECRET_KEY)
 app.use(express.json())
 const port=process.env.PORT || 4000
 app.get('/',(req,res)=>{
@@ -18,13 +20,13 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 function verifyJWT(req, res, next) {
   const authHeader = req.headers.authorization;
-  console.log(authHeader)
+
   if (!authHeader) {
     return res.status(401).send({ message: 'UnAuthorized access' });
   }
   const token = authHeader.split(' ')[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
-    console.log(decoded)
+   
     if (err) {
       return res.status(403).send({ message: 'Forbidden access' })
     }
@@ -40,6 +42,7 @@ const run=async()=>{
     const bookingCollection=client.db('doctors_portal').collection('bookings');
     const userCollection=client.db('doctors_portal').collection('users');
     const doctorsCollection=client.db('doctors_portal').collection('doctors');
+    const paymentCollection=client.db('doctors_portal').collection('payments');
 
 
 
@@ -52,7 +55,29 @@ const run=async()=>{
         res.status(403).send({message:'forbidden'})
       }
     }
-
+//payments=====================================
+// app.post('/create-payment-intent',verifyJWT, async(req,res)=>{
+//   const service=req.body 
+//   const price=service.price 
+//   const amount=price*100; 
+//   const paymentIntent=await stripe.paymentIntents.create({
+//     amount:amount,
+//     currency:'usd',
+//     payment_method_types:['card']
+//   })
+//   res.send({clientSecret:paymentIntent.client_secret})
+// })
+app.post('/create-payment-intent', verifyJWT, async(req, res) =>{
+  const service = req.body;
+  const price = service.price;
+  const amount = price*100;
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount : amount,
+    currency: 'usd',
+    payment_method_types:['card']
+  });
+  res.send({clientSecret: paymentIntent.client_secret})
+});
 
   app.get('/admin/:email',async(req,res)=>{
     const email=req.params.email
@@ -130,6 +155,14 @@ const run=async()=>{
       }
     })
 
+
+    app.get('/booking/:id',verifyJWT,async(req,res)=>{
+      const id=req.params.id 
+      const query={_id:ObjectId(id)}
+      const booking=await bookingCollection.findOne(query) 
+      res.send(booking)
+    })
+
     app.post('/booking',async(req,res)=>{
       const booking=req.body 
       const query={treatment:booking.treatment,date:booking.date,patient:booking.patient}
@@ -143,6 +176,24 @@ const run=async()=>{
       }
     
     });
+
+
+    // payment booking 
+    app.patch('/booking/:id',verifyJWT,async(req,res)=>{
+      const id=req.params.id 
+      const payment=req.body
+      console.log(payment,"transaction id")
+      const filter={_id:ObjectId(id)}
+      const updateDoc={
+        $set:{
+          paid:true,
+          transactionId:payment.transactionId
+        }
+      }
+      const updatedBooking=await bookingCollection.updateOne(filter,updateDoc);
+      const result=await paymentCollection.insertOne(payment)
+      res.send(updateDoc)
+    })
 
     app.get('/available',async(req,res)=>{
       const date=req.query.date || "May 14, 2022"
